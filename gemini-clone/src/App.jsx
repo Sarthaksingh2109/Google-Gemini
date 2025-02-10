afee7820189c49294e97e6b3d841d9eba8d2e860
Sarthaksingh2109 <sarthaksingh9452@gmail.com>
2025-02-10 15:11:21 +0530

    Build a Generative AI application using Google Gemini API with React JS, enabling seamless integration of AI features and interactive user experiences through intuitive design and functionality.

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./componets/sidebar/Sidebar";
+import Main from "./componets/main/Main";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default App;
